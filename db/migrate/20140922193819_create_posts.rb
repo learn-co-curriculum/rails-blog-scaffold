2911eefddb391c3f680c66251cc65f213a2bb0e2
maxwellbenton <maxwellbenton@gail.com>
2018-08-29 09:33:58 -0400

    updated solution, rebase from master

--- db/migrate/20140922193819_create_posts.rb
+++ db/migrate/20140922193819_create_posts.rb
@@ -2,8 +2,9 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
+      t.string :description
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
